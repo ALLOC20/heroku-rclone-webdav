383b4bfa22eabf48cf532c89691ab7fec1520540
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-08-21 10:15:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-# 初始化rclone配置
-mkdir -p /.config/rclone
-cat << EOF > /.config/rclone/rclone.conf
+# 初始化rclone.conf配置
+cat << EOF > /.rclone.conf
 $rclone
 EOF
 # 启动rclone
-exec /usr/bin/rclone serve webdav ${parameter}  --addr :${PORT}
+exec /usr/bin/rclone serve webdav ${parameter}
